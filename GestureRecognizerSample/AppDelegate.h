34eba2b652accce1e7b6e771f44c6eb1425f6eb9
Sumardi Shukor <me@sumardi.net>
2012-05-02 15:05:10 +0800

    Initial Commit

---
+++ GestureRecognizerSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GestureRecognizerSample
+//
+//  Created by Sumardi Shukor on 5/2/12.
+//  Copyright (c) 2012 Wutmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GestureRecognizerSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GestureRecognizerSample
+//
+//  Created by Sumardi Shukor on 5/2/12.
+//  Copyright (c) 2012 Wutmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
